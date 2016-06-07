615b1190ffe07738e8d437c6f7a0cee6a32cc998
dazinator <darrell.tunnell@googlemail.com>
2016-06-07 17:11:18 +0100

    closes #22 Now deletes project.lock.json files after a successful upgrade.

--- src/AspNetUpgrade/AspNetUpgrade/Program.cs
+++ src/AspNetUpgrade/AspNetUpgrade/Program.cs
@@ -38,6 +38,14 @@ namespace AspNetUpgrade
                   // save changes to disk.
                   solutionUpgradeContext.SaveChanges();
 
+                  Console.WriteLine("Deleting project.lock.json files..");
+                  var projectLockFiles = solutionDir.GetFileSystemInfos("project.lock.json", SearchOption.AllDirectories);
+                  foreach (var projectLock in projectLockFiles)
+                  {
+                      Console.WriteLine("Deleting lock file  " + projectLock.ToString());
+                      projectLock.Delete();
+                  }
+
                   if (options.Verbose)
                   {
                       Console.WriteLine("Upgrade finished.");
